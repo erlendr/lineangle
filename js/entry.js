1f938beebd82606921e897f5a6c5fb3beb6644e0
Erlend Røsjø <erlend@erlend.net>
2015-03-13 12:48:35 +0100

    Working code

--- js/entry.js
+++ js/entry.js
@@ -5,9 +5,9 @@
     showCopyright: false,
   });
 
-  var pointA = [2,8];
-  var pointB = [-5,1];
-  var pointC = [5,0];
+  var pointA = [5,0];
+  var pointB = [9,8];
+  var pointC = [-5,1];
 
   //Add points for triangle
   var pointAPoint = board.create('point', pointA, {
@@ -25,21 +25,40 @@
     strokecolor: 'red',
   });
 
-  var lineAB = board.create('line', [pointAPoint, pointBPoint], {
-    straightFirst: false,
-    straightLast: false, 
-    strokeWidth: 2,
-  });
-
   var lineAC = board.create('line', [pointAPoint, pointCPoint], {
     straightFirst: false,
     straightLast: false, 
     strokeWidth: 2,
   });
 
-  var lineBC = board.create('line', [pointBPoint, pointCPoint], {
+  var lineAB = board.create('line', [pointAPoint, pointBPoint], {
     straightFirst: false,
     straightLast: false, 
     strokeWidth: 2,
   });
+
+  var lengthAB = distance(pointAPoint.X(), pointAPoint.Y(), pointBPoint.X(), pointBPoint.Y());
+  var lengthAC = distance(pointAPoint.X(), pointAPoint.Y(), pointCPoint.X(), pointCPoint.Y());
+  var lengthBC = distance(pointBPoint.X(), pointBPoint.Y(), pointCPoint.X(), pointCPoint.Y());
+
+  var angle1 = angle(lengthAB, lengthAC, lengthBC);
+  board.create('text',[pointA[0]+1, pointA[1]+1, function() { 
+   var lengthAB = distance(pointAPoint.X(), pointAPoint.Y(), pointBPoint.X(), pointBPoint.Y());
+   var lengthAC = distance(pointAPoint.X(), pointAPoint.Y(), pointCPoint.X(), pointCPoint.Y());
+   var lengthBC = distance(pointBPoint.X(), pointBPoint.Y(), pointCPoint.X(), pointCPoint.Y());
+
+   return 'Angle: ' + Math.round(degrees(angle(lengthAB, lengthAC, lengthBC))) + ' deg.';
+ }], {fontSize:10});
 })();
+
+function distance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2-x1, 2) +Math.pow(y2-y1, 2)); 
+}
+
+function angle(d1, d2, d3) {
+  return Math.acos((Math.pow(d1, 2) + Math.pow(d2, 2) - Math.pow(d3, 2)) / (2 * d1 * d2)); 
+}
+
+function degrees(radians) {
+  return radians * 180 / Math.PI;
+}
