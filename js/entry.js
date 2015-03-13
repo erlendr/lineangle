b6e8fda800eb104877694c1e7eb361ce80b96bb0
Erlend Røsjø <erlend@erlend.net>
2015-03-13 14:45:13 +0100

    interactive merge-like stuff

--- js/entry.js
+++ js/entry.js
@@ -6,8 +6,9 @@
   });
 
   var pointA = [5,0];
-  var pointB = [9,8];
+  var pointB = [1,7];
   var pointC = [-5,1];
+  var pointD = [5,7];
 
   //Add points for triangle
   var pointAPoint = board.create('point', pointA, {
@@ -25,6 +26,11 @@
     strokecolor: 'red',
   });
 
+  var pointDPoint = board.create('point', pointD, {
+    name: 'D',
+    strokecolor: 'red',
+  });
+
   var lineAC = board.create('line', [pointAPoint, pointCPoint], {
     straightFirst: false,
     straightLast: false, 
@@ -37,19 +43,52 @@
     strokeWidth: 2,
   });
 
-  var lengthAB = distance(pointAPoint.X(), pointAPoint.Y(), pointBPoint.X(), pointBPoint.Y());
-  var lengthAC = distance(pointAPoint.X(), pointAPoint.Y(), pointCPoint.X(), pointCPoint.Y());
-  var lengthBC = distance(pointBPoint.X(), pointBPoint.Y(), pointCPoint.X(), pointCPoint.Y());
+  var lineAD = board.create('line', [pointAPoint, pointDPoint], {
+    straightFirst: false,
+    straightLast: false, 
+    strokeWidth: 2,
+  });
 
-  var angle1 = angle(lengthAB, lengthAC, lengthBC);
+
   board.create('text',[pointA[0]+1, pointA[1]+1, function() { 
-   var lengthAB = distance(pointAPoint.X(), pointAPoint.Y(), pointBPoint.X(), pointBPoint.Y());
-   var lengthAC = distance(pointAPoint.X(), pointAPoint.Y(), pointCPoint.X(), pointCPoint.Y());
-   var lengthBC = distance(pointBPoint.X(), pointBPoint.Y(), pointCPoint.X(), pointCPoint.Y());
+    return 'CAB Angle: ' + Math.round(degrees(lineangle(pointAPoint, pointBPoint, pointCPoint))) + ' deg.';
+  }], {fontSize:12});
 
-   return 'Angle: ' + Math.round(degrees(angle(lengthAB, lengthAC, lengthBC))) + ' deg.';
- }], {fontSize:10});
+  board.create('text',[pointA[0]-1, pointA[1]-1, function() { 
+   return 'CAD Angle: ' + Math.round(degrees(lineangle(pointAPoint, pointCPoint, pointDPoint))) + ' deg.';
+ }], {fontSize:12});
+
+  cabLine = board.create('line', [pointCPoint, pointBPoint], {visible: false});
+  cadLine = board.create('line', [pointCPoint, pointDPoint], {visible: false});
+
+  pointAPoint.on('drag', cb);
+  pointBPoint.on('drag', cb);
+  pointCPoint.on('drag', cb);
+  pointDPoint.on('drag', cb);
+
+  function cb() {
+    cabLine.hideElement();
+    cadLine.hideElement();
+
+    var cabAngle = lineangle(pointAPoint, pointBPoint, pointCPoint);
+    var cadAngle = lineangle(pointAPoint, pointCPoint, pointDPoint);
+    if(cabAngle < cadAngle) {
+      cabLine.showElement();
+    }
+    else {
+      cadLine.showElement(); 
+    }
+  }
+
 })();
+
+function lineangle(pA, pB, pC) {
+  var lengthAB = distance(pA.X(), pA.Y(), pB.X(), pB.Y());
+  var lengthAC = distance(pA.X(), pA.Y(), pC.X(), pC.Y());
+  var lengthBC = distance(pB.X(), pB.Y(), pC.X(), pC.Y());
+
+  return angle(lengthAB, lengthAC, lengthBC);  
+}
 
 function distance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(x2-x1, 2) +Math.pow(y2-y1, 2)); 
